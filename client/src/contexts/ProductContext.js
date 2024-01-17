f9cc6ad0c156bacfadf6ef1a84d14a2a1c7c0be3
Alicja Chmura <ala.chmura2003@gmail.com>
2024-01-17 19:38:50 +0100

    Changed Product Context to use custom useFetch hook

--- client/src/contexts/ProductContext.js
+++ client/src/contexts/ProductContext.js
@@ -1,17 +1,27 @@
-import React, { createContext, useState } from 'react';
-import dummyProducts from '../data/dummyProducts.json'
+import React, { createContext, useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
 export const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
-    // Just for now to focus on creating data displaying components - later change to fetch the data
-    const initialProducts = dummyProducts.products
-    const [products, setProducts] = useState(initialProducts);
+    const { data, loading, error } = useFetch(
+      'http://localhost:4000/api/products'
+    );
+    
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      if (!loading && !error) {
+        setProducts(data.products);
+      }
+    }, [loading, error, data]);
 
     return (
     <ProductContext.Provider
         value={{
         products,
         setProducts,
+        loading,
+        error
         }}
     >
         {children}
